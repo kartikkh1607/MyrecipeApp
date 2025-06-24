3f837b7e442f0a5c4fe03222a8b16e80e09b6016
kartikkh1607 <kartikkhandelwal1234589@gmail.com>
2025-06-24 21:47:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myrecipeapp/ApiServices.kt
@@ -0,0 +1,27 @@
+package com.example.myrecipeapp
+
+// end goal is “Hey Retrofit! Go to www.themealdb.com/api/json/v1/1/categories.php,
+// get me the data, convert it into nice Kotlin objects, and I’ll use it in my app.”
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+// This creates a Retrofit machine — like a delivery guy who knows where to go,
+// and how to bring the food (data) back.
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl("https://www.themealdb.com/api/json/v1/1/")           // this is the website
+    .addConverterFactory(GsonConverterFactory.create())    // convert json to kotlin
+    .build()                                               // puts everything together
+
+// Hey Retrofit, here's the menu (ApiServices)
+// — please give me a working waiter (recepieService) who knows how to go and get the data.”
+
+val recepieService = retrofit.create(ApiServices::class.java)
+
+interface ApiServices{
+    @GET("categories.php")                            // go to this page on this website
+    suspend fun getCategories() : CategoriesResponse  // fetch the data and (wait for it) and get something like
+                                                      // CategoriesResponse
+}
---
+++ app/src/main/java/com/example/myrecipeapp/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.myrecipeapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    data class RecipeState(
+        val loading : Boolean = true,
+        val list : List<Category> = emptyList(),
+        val error : String ?= null
+    )
+
+    // We’re locking the real data inside the ViewModel
+    // and only giving UI a copy to look at (read-only).
+
+    private val _CategoriesState = mutableStateOf(RecipeState()) // private box that stores all the UI info
+    val CategoriesState : State<RecipeState> = _CategoriesState
+
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories(){
+        viewModelScope.launch {     //  Starts a coroutine (background thread
+            try {
+                val response = recepieService.getCategories()
+                _CategoriesState.value = _CategoriesState.value.copy(
+                    list = response.categories,
+                    loading = false,
+                    error = null
+                )
+
+            }catch(e: Exception) {
+                _CategoriesState.value = _CategoriesState.value.copy(
+                    loading = false,
+                    error = ("error fetching Categories ${e.message}")
+                )
+            }
+        }
+    }
+
+}
